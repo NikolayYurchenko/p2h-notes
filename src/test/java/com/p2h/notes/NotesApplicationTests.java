c8d068f351120e43902161cd18677e6f80f8ef82
nikolay_yurchenko <kolya3864@gmail.com>
2023-02-15 01:33:36 +0200

    Initial commit

---
+++ src/test/java/com/p2h/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2h.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
